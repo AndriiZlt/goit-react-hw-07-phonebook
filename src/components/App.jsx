f43607d5b5bf7136d16edf9948691606357ba935
AndriiZlt <andreyzlot@gmail.com>
2023-07-09 18:36:29 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './Contacts/Contacts';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 // import Loading from './Loading/Loading';
 
 export default function App() {
   // const error = useSelector(state => state.error);
-  // const isLoading = useSelector(state => state.contacts.isLoading);
 
   return (
     <div
@@ -41,7 +40,6 @@ export default function App() {
         >
           <h2 style={{ margin: 0, fontSize: 18 }}>Contacts:</h2>
           <Filter />
-          {/* {isLoading ? <div>Loading...</div> : <ContactList />} */}
           <ContactList />
         </div>
       </div>
